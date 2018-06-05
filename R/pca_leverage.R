3a9b88815cef1df9e8d56eca76c41214a648633a
muschellij2 <muschellij2@gmail.com>
2018-06-05 11:39:34 -0400

    Added the citation and other functionality.

--- R/pca_leverage.R
+++ R/pca_leverage.R
@@ -1,6 +1,9 @@
 #' Calculate the Leverage for a Principal Components Analysis
 #'
 #' @param x data matrix of values, passed to \code{\link{fast.prcomp}}
+#' @param center logical value for centering, passed to \code{\link{prcomp}}
+#' @param scale. logical value for scaling, passed to \code{\link{prcomp}}
+#'
 #'
 #' @return A list of components and the leverage
 #' @export
@@ -9,6 +12,9 @@
 #' @examples
 #' x = matrix(rnorm(100*3), ncol = 3)
 #' lev = pca_leverage(x)
-pca_leverage = function(x) {
+pca_leverage = function(x,
+                        center = TRUE,
+                        scale. = FALSE
+                        ) {
   res = fast.prcomp(x)
 }
