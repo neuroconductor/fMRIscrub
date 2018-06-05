eacc54cdd9478eacd82102194465227f5247b3ec
muschellij2 <muschellij2@gmail.com>
2018-06-05 12:09:10 -0400

    updated pca_leverage stuff

--- R/pca_leverage.R
+++ R/pca_leverage.R
@@ -3,18 +3,23 @@
 #' @param x data matrix of values, passed to \code{\link{fast.prcomp}}
 #' @param center logical value for centering, passed to \code{\link{prcomp}}
 #' @param scale. logical value for scaling, passed to \code{\link{prcomp}}
-#'
+#' @param ... additional arguments passed to \code{\link{fast.prcomp}}
 #'
 #' @return A list of components and the leverage
 #' @export
 #'
 #' @importFrom gmodels fast.prcomp
 #' @examples
-#' x = matrix(rnorm(100*3), ncol = 3)
+#' n_voxels = 1e5
+#' n_timepoints = 100
+#' x = matrix(rnorm(n_timepoints*n_voxels), ncol = n_voxels)
+#'
 #' lev = pca_leverage(x)
 pca_leverage = function(x,
                         center = TRUE,
-                        scale. = FALSE
+                        scale. = FALSE,
+                        ...
                         ) {
-  res = fast.prcomp(x)
+  res = fast.prcomp(x, center = center,
+                    scale. = scale)
 }
