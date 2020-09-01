91b70b9416fb9d49e9155e53a07f7bea9c6476e4
Damon Pham <damondpham@gmail.com>
2020-09-01 11:41:06 -0700

    Update PCATF.R

--- R/PCATF.R
+++ R/PCATF.R
@@ -7,7 +7,7 @@
 #'	will be needed to display the leverage images for outlying observations.
 #' @param K (Optional) The number of PCs to solve for. If not provided, the
 #'	number of PCs will be the amount of regular PCs with variance above
-#'	the mean, up to 100 PCs. 
+#'	the mean, up to 100 PCs.
 #' @param lambda The trend filtering parameter; roughly, the filtering intensity.
 #'	Default is 0.05 . Can be NULL (lets algorithm decide).
 #' @param niter_max The number of iterations to use for approximating the PC.
@@ -17,10 +17,28 @@
 #' @return SVD The trend-filtered SVD decomposition of X (list with u, d, v).
 #'
 #' @importFrom glmgen trendfilter
-#' @importFrom far orthonormalization
+#' @example
+#' set.seed(12345)
+#' U = matrix(rnorm(100*3),ncol=3)
+#' U[20:23,1] = U[20:23,1] + 3
+#' U[40:43,2] = U[40:43,2] - 2
+#' U = svd(U)$u
+#' D = diag(c(10,5,1))
+#' V = svd(matrix(rnorm(3*20),nrow=20))$u
+#' X = U %*% D %*% t(V)
+#' out3 = PCATF(X, K=3, lambda=.75)
+#' matplot(out3$u, ty='l')
+#' out3$d
+#' plot(rowSums(out3$u^2), ty='l')
+#'
+#' # Orthonormalized
+#' out3_svd = svd(out3$u)
+#' matplot(out3_svd$u, ty='l')
+#' out3_svd$d
+#' plot(rowSums(out3_svd$u^2), ty='l')
 #' @export
 PCATF <- function(X, X.svd=NULL, solve_directions = TRUE, K=NULL, lambda=.05,
-  niter_max = 1000, TOL = 1e-8, verbose=FALSE){
+                  niter_max = 1000, TOL = 1e-8, verbose=FALSE){
 
   stopifnot(is.numeric(X))
   if(is.null(X.svd)){ X.svd <- svd(X) }
@@ -29,11 +47,11 @@ PCATF <- function(X, X.svd=NULL, solve_directions = TRUE, K=NULL, lambda=.05,
   if(is.null(K)){
     K <- length(choose_PCs.variance(X.svd, max_keep=NULL, min_keep=NULL))
     K <- min(100, K)
-  } 
+  }
   stopifnot(is.numeric(K))
   stopifnot(K==round(K))
   stopifnot(is.numeric(lambda))
-  if(lambda == 0){ 
+  if(lambda == 0){
     return(
       list(u = matrix(X.svd$u[, 1:K], ncol=K),
            d = X.svd$d[1:K],
@@ -64,25 +82,21 @@ PCATF <- function(X, X.svd=NULL, solve_directions = TRUE, K=NULL, lambda=.05,
     # Iterate between trendfiltering u and orthonormalizing v.
     for(i in 1:niter_max){
       u.last <- u
-      tf.kwargs <- list(y = scale(X %*% v, center = FALSE, scale = d),
-      x = 1:T_, nlambda = 1, k = 0)
+      tf.kwargs <- list(y = X %*% v, #scale(X %*% v, center = FALSE, scale = d),
+                        x = 1:T_, nlambda = 1, k = 0)
       if(!is.null(lambda)){ tf.kwargs$lambda <- lambda }
       u <- do.call(glmgen::trendfilter, tf.kwargs)$beta
-      if(any(is.na(u))){ 
+      u <- u / sqrt(sum(u^2))
+      if(any(is.na(u))){
         u <- u.last
         break
       }
-      v <- tryCatch({
-        far::orthonormalization(crossprod(X, u), basis = FALSE, norm = TRUE)
-      }, error = function(e) {
-        # colinears vectors error
-        if(verbose){print(paste0('Co-linear vectors: PC ', k, ' did not converge.'))}
-        return(v)
-      })
+      v <- crossprod(X, u)
+      v <- v / sqrt(sum(v^2))
       diff <- sqrt(mean((u - u.last)^2))
       if(diff < TOL){ break }
       if(verbose & i == niter_max){
-        print(paste0('PC ', k, ' did not converge.'))
+        cat(paste0('PC ', k, ' did not converge.\n'))
       }
     }
 
