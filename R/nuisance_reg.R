a40103046d4e222d27f6cd0bfb521f59597733bc
Damon Pham <ddpham@i27.red.uits.iu.edu>
2020-11-08 02:56:17 -0500

    fixes

--- R/nuisance_reg.R
+++ R/nuisance_reg.R
@@ -2,17 +2,20 @@
 #'
 #' Regresses matrix of nuisance variables from data matrix
 #'
-#' @param data \eqn{N \times P} matrix of N measurements/timepoints, P data variables
+#' @param dat \eqn{N \times P} matrix of N measurements/timepoints, P data variables
 #' @param nuis \eqn{N \times Q} matrix of N measurements/timepoints, Q nuisance regressors
 #' @param intercept Add intercept column (constant) to \code{nuis}? Default: \code{TRUE}.
-#' @return \code{data} with \code{nuis} regressed from it
+#' @return \code{dat} with \code{nuis} regressed from it
 #' @export
-nuisance_reg <- function(data, nuis, intercept=TRUE){
-  data <- as.matrix(data); nuis <- as.matrix(nuis)
-  stopifnot(nrow(data) == nrow(nuis))
-  
-  if (intercept) { nuis <- cbind(1, nuis) }
-  nuis <- apply(nuis, 2, scale) # I think this would help numeric stability.
+nuisance_reg <- function(dat, nuis, intercept=TRUE){
+  dat <- as.matrix(dat); nuis <- as.matrix(nuis)
+  stopifnot(nrow(dat) == nrow(nuis))
+
+  # Scale: I think this would help numeric stability.
+  nuis <- scale(nuis)
+
+  if (intercept) { nuis <- cbind(1/nrow(nuis), nuis) }
+
   I_mH <- diag(1200) - nuis %*% solve(t(nuis) %*% nuis, t(nuis))
-  I_mH %*% data
+  I_mH %*% dat
 }
