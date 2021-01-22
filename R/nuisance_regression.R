1e24b51b7a25fdcc07b2a12290312276be40fbf6
Damon Pham <damondpham@gmail.com>
2021-01-22 07:41:29 -0800

    fix nuisance_regression R in case of collinear bases

--- R/nuisance_regression.R
+++ R/nuisance_regression.R
@@ -14,6 +14,7 @@ nuisance_regression <- function(X, design){
   # https://stackoverflow.com/questions/39167204/in-r-how-does-one-extract-the-hat-projection-influence-matrix-or-values-from-an
   qrd <- qr(design)
   design <- design[, qrd$pivot[seq_len(qrd$rank)]]
+  qrd <- qr(design)
   Qd <- qr.Q(qrd)
   I_m_H <- diag(nrow(design)) - (Qd %*% t(Qd))
   if (nrow(X)==nrow(design)) {
