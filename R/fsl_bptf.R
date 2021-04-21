33c8ed5acacacf5b3f87bb083777d8c2e28c0246
Damon Pham <damondpham@gmail.com>
2021-04-20 23:02:02 -0700

    Export `fsl_bptf`

--- R/fsl_bptf.R
+++ R/fsl_bptf.R
@@ -4,8 +4,11 @@
 #'  https://cpb-us-w2.wpmucdn.com/sites.udel.edu/dist/7/4542/files/2016/09/fsl_temporal_filt-15sywxn.m
 #'  https://github.com/rordenlab/niimath/blob/master/src/core32.c
 #' 
-#' @param orig_data T_ x V_ data matrix whose columns will be detrended
+#' @param orig_data \eqn{T \times V} data matrix whose columns will be detrended
 #' @param HP_sigma The frequency parameter for the highpass filter
+#' 
+#' @export
+#' 
 fsl_bptf <- function(orig_data, HP_sigma=2000) {
   T_ <- nrow(orig_data)
 
