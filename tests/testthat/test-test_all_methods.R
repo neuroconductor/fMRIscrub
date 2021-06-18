b140d994c6faa0ba18e968571558b350c7c94caf
Damon Pham <damondpham@gmail.com>
2021-06-18 06:38:54 -0700

    Add testthat::expect_warning wrapper to pass tests

--- tests/testthat/test-test_all_methods.R
+++ tests/testthat/test-test_all_methods.R
@@ -1,20 +1,20 @@
 test_that("clever works with all methods and default settings", {
   data(Dat1)
-  clev <- fMRIscrub:::clever_multi(
+  clev <- testthat::expect_warning(fMRIscrub:::clever_multi(
     Dat1,
     projection = "all"
-  )
+  ))
   #myplot <- plot(clev, "all")
 })
 
 test_that("clever works with some custom parameter settings", {
   data(Dat2)
-  clev <- fMRIscrub:::clever_multi(
+  clev <- testthat::expect_warning(fMRIscrub:::clever_multi(
     Dat2,
     projection = c("PCATF", "ICA_kurt"),
     kurt_quantile = .90,
     cutoff = 5,
     verbose = TRUE
-  )
+  ))
   #myplot <- plot(clev, "all", title = "My Plot")
 })
