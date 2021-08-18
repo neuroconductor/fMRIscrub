fb1b50d41ff2bc2f0a8379ba58e13fa8455c3426
Damon Pham <damondpham@gmail.com>
2021-08-17 20:18:24 -0400

    add info about Dat1 and Dat2

--- R/data.R
+++ R/data.R
@@ -1,13 +1,27 @@
-#' Example fMRI Time Series for Subject 1
+#' First Example Time Series from the ABIDE
 #'
-#' A masked slice of an fMRI time series, turned into a matrix.
+#' A sagittal slice from the fMRI time series for subject 0050048 (Pitt). The
+#'  data has been pre-processed with slice time correction, rigid body 
+#'  realignment estimation, spatial normalization to MNI space, and linear 
+#'  detrending. Subject 0050048 was a typically-developing 11-year-old male. 
+#'  The scan has many artifacts. A mask was applied to vectorize the spatial 
+#'  dimensions. 
+#' 
+#'  Source: http://fcon_1000.projects.nitrc.org/indi/abide/abide_I.html
 #'
-#' @format A matrix of 193 time points by 4675 voxels
+#' @format A numeric matrix of 193 time points by 4675 voxels
 "Dat1"
 
-#' Example fMRI Time Series for Subject 2
+#' Second Example Time Series from the ABIDE
 #'
-#' A masked slice of an fMRI time series, turned into a matrix
-#'
-#' @format A matrix of 145 time points by 4679 voxels
+#' A sagittal slice from the fMRI time series for subject 0051479 (Caltech). The
+#'  data has been pre-processed with slice time correction, rigid body 
+#'  realignment estimation, spatial normalization to MNI space, and linear 
+#'  detrending. Subject 0051479 was a typically-developing 20-year-old female. 
+#'  The scan has few to no artifacts. A mask was applied to vectorize the spatial 
+#'  dimensions. 
+#' 
+#'  Source: http://fcon_1000.projects.nitrc.org/indi/abide/abide_I.html
+#' 
+#' @format A numeric matrix of 145 time points by 4679 voxels
 "Dat2"
