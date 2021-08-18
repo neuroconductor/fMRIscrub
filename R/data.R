4c1c2c56141041a9f87e947c1f7fd46d7f6e91a7
Damon Pham <damondpham@gmail.com>
2021-08-17 20:41:15 -0400

    edits part 1

--- R/data.R
+++ R/data.R
@@ -1,10 +1,11 @@
 #' First Example Time Series from the ABIDE
 #'
-#' A sagittal slice from the fMRI time series for subject 0050048 (Pitt). The
-#'  data has been pre-processed with slice time correction, rigid body 
-#'  realignment estimation, spatial normalization to MNI space, and linear 
-#'  detrending. Subject 0050048 was a typically-developing 11-year-old male. 
-#'  The scan has many artifacts. A mask was applied to vectorize the spatial 
+#' A sagittal slice from the fMRI time series for subject 0050048. The scan was
+#'  obtained at the University of Pittsburgh School of Medicine. The scan has
+#'  been pre-processed with slice time correction, rigid body realignment
+#'  estimation, spatial normalization to MNI space, and linear detrending. 
+#'  Subject 0050048 was a typically-developing 11-year-old male. The scan has
+#'  many artifacts. A mask was applied to vectorize the spatial 
 #'  dimensions. 
 #' 
 #'  Source: http://fcon_1000.projects.nitrc.org/indi/abide/abide_I.html
@@ -14,11 +15,12 @@
 
 #' Second Example Time Series from the ABIDE
 #'
-#' A sagittal slice from the fMRI time series for subject 0051479 (Caltech). The
-#'  data has been pre-processed with slice time correction, rigid body 
-#'  realignment estimation, spatial normalization to MNI space, and linear 
-#'  detrending. Subject 0051479 was a typically-developing 20-year-old female. 
-#'  The scan has few to no artifacts. A mask was applied to vectorize the spatial 
+#' A sagittal slice from the fMRI time series for subject 0051479. The scan was 
+#'  obtained at the California Institute of Technology. The scan has
+#'  been pre-processed with slice time correction, rigid body realignment
+#'  estimation, spatial normalization to MNI space, and linear detrending. 
+#'  Subject 0051479 was a typically-developing 20-year-old female. The scan has 
+#'  few visible artifacts. A mask was applied to vectorize the spatial 
 #'  dimensions. 
 #' 
 #'  Source: http://fcon_1000.projects.nitrc.org/indi/abide/abide_I.html
